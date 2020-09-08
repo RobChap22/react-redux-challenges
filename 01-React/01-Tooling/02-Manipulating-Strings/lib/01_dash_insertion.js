cd323706c7d5880041c42e79de5e579a7364ea24
Rob Chapman <autosexual@gmail.com>
2020-09-08 17:58:45 +0900

    need to commit

--- 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
+++ 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
@@ -1,6 +1,13 @@
 function insertDash(word) {
   // TODO: implement the method and return word with dashes
-  return null;
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const wordArr = word.split("");
+  for (let i = 1; i < wordArr.length; i++) {
+    if (vowels.includes(wordArr[i - 1]) && vowels.includes(wordArr[i]) {
+      wordArr.splice(i, 0, '-');
+    }
+  }
+  return wordArr.join('');
 }
 
 module.exports = insertDash;
